93293aac9ff3958329a11f2b48cdfaa223cfbff1
Mustafa <mustafaislamovich@gmail.com>
2022-12-04 02:02:17 +0100

    init tailwindcss, made sidenavbar and adjusted body on dashboard

---
+++ components/Dashboard.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='lg:ml-64 mt-6 max-w-[150px] flex gap-6'>
+            <div className='flex justify-between bg-white p-4'>
+            <div>
+            <div className='card-team'>
+                NYK
+            </div>
+            <div className='result-team'>
+                100
+            </div>
+            </div>
+            <div>
+            <div className='card-team'>
+                OKC
+            </div>
+            <div className='result-team'>
+                103
+            </div>
+            </div>
+            </div>
+
+
+            <div className='flex justify-between bg-white p-4'>
+            <div>
+            <div className='card-team'>
+                NYK
+            </div>
+            <div className='result-team'>
+                100
+            </div>
+            </div>
+            <div>
+            <div className='card-team'>
+                OKC
+            </div>
+            <div className='result-team'>
+                103
+            </div>
+            </div>
+            </div>
+
+
+
+    </div>
+  )
+}
+
+export default Dashboard
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import Sidebar from '../components/Sidebar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
